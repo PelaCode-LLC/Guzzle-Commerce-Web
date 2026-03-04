a4cf1f52331c74e7fc3cdbf23f462ff6fe6dce95
Cody Miller <codymiller1771@gmail.com>
2026-03-03 20:53:55 -0600

    Add guzzle logo

--- src/components/Logo/Logo.example.js
+++ src/components/Logo/Logo.example.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import scaled24h from './images/scaled24h.png';
-import scaled24h2x from './images/scaled24h2x.png';
-import scaled36h from './images/scaled36h.png';
-import scaled36h2x from './images/scaled36h2x.png';
-import scaled48h from './images/scaled48h.png';
-import scaled48h2x from './images/scaled48h2x.png';
+import scaled24h from './images/Guzzle Logo.png';
+import scaled24h2x from './images/Guzzle Logo.png';
+import scaled36h from './images/Guzzle Logo.png';
+import scaled36h2x from './images/Guzzle Logo.png';
+import scaled48h from './images/Guzzle Logo.png';
+import scaled48h2x from './images/Guzzle Logo.png';
 
 import { LogoComponent } from './Logo';
 import css from './LinkedLogo.module.css';
