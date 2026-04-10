ba6045b5215d3cbf4874e73556e95a5eb34aab9f
Cody Miller <codymiller1771@gmail.com>
2026-04-10 12:30:18 -0500

    more polishing

--- src/containers/LandingPage/FallbackPage.js
+++ src/containers/LandingPage/FallbackPage.js
@@ -14,13 +14,34 @@ const PageBuilder = loadable(() =>
 const CATEGORY_PARAM_NAME = 'pub_categoryLevel1';
 const fallbackCategories = localCategoryCards;
 const categoryIconById = {
-  'cars & trucks': 'CT',
-  motorcycles: 'MC',
-  watercraft: 'WC',
-  parts: 'PT',
-  electronics: 'EL',
-  tools: 'TL',
-  services: 'SV',
+  'cars & trucks': (
+    <path d="M7 15l1.6-4.4A2.4 2.4 0 0110.86 9h6.28a2.4 2.4 0 012.26 1.6L21 15M6 17h16M9 17a1.5 1.5 0 103 0 1.5 1.5 0 00-3 0zm6 0a1.5 1.5 0 103 0 1.5 1.5 0 00-3 0zM8.2 15h7.6" />
+  ),
+  motorcycles: (
+    <path d="M7.5 17.5a2.5 2.5 0 105 0 2.5 2.5 0 00-5 0zm9 0a2.5 2.5 0 105 0 2.5 2.5 0 00-5 0zM10 17.5h6M12 11l2.2 1.8h2.2l1.6 2.2M10.8 11H15l-1.4-2H11" />
+  ),
+  watercraft: (
+    <path d="M5 16.5c1.3 0 1.3.8 2.7.8s1.4-.8 2.8-.8 1.4.8 2.8.8 1.4-.8 2.8-.8 1.4.8 2.8.8 1.5-.8 2.9-.8M7 14h10l-2.1-3.3a1.8 1.8 0 00-1.5-.8H11l-1.5 1.7H7z" />
+  ),
+  parts: <path d="M10.5 8.5l3-3 2 2-3 3m-2 2l-3 3-2-2 3-3m4-2l2 2m-7 7l8-8" />,
+  electronics: (
+    <path d="M9 8h6v8H9zM7 10H5m14 0h-2M7 14H5m14 0h-2M10 6V4m4 2V4m-4 14v2m4-2v2" />
+  ),
+  tools: <path d="M9 8l7 7M8 11l3-3 5 5-3 3-5-5zm8-5l2 2-2.5 2.5-2-2z" />,
+  services: (
+    <path d="M12 7h6a2 2 0 012 2v8a2 2 0 01-2 2H8a2 2 0 01-2-2V9a2 2 0 012-2h1m3-2l3 3-3 3M9 5h3v6" />
+  ),
+};
+
+const CategoryIcon = props => {
+  const { categoryId } = props;
+  const path = categoryIconById[categoryId] || <circle cx="12" cy="12" r="5" />;
+
+  return (
+    <svg className={css.categoryIconSvg} viewBox="0 0 24 24" aria-hidden="true">
+      {path}
+    </svg>
+  );
 };
 
 // Create fallback content (array of sections) in page asset format:
@@ -125,7 +146,6 @@ const SectionCategoryLinks = props => {
           {categoryCards.map(category => {
             const search = `?${CATEGORY_PARAM_NAME}=${encodeURIComponent(category.id)}`;
             const categoryName = category.name || category.label || category.id;
-            const categoryIcon = categoryIconById[category.id] || 'IT';
 
             return (
               <NamedLink
@@ -135,7 +155,9 @@ const SectionCategoryLinks = props => {
                 className={css.categoryCard}
               >
                 <span className={css.categoryMain}>
-                  <span className={css.categoryIcon}>{categoryIcon}</span>
+                  <span className={css.categoryIcon}>
+                    <CategoryIcon categoryId={category.id} />
+                  </span>
                   <span className={css.categoryName}>{categoryName}</span>
                 </span>
                 <span className={css.categoryAction}>
